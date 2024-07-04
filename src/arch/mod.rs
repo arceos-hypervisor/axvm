fd9f21b19fb4560663d326b1bf05b6f402e70f84
PeizhongQiu <15608652022@163.com>
2024-07-04 10:07:09 +0800

    gpm, vcpu

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -4,5 +4,8 @@ cfg_if::cfg_if! {
     if #[cfg(target_arch = "x86_64")] {
         mod x86_64;
         pub use self::x86_64::*;
+    } else if #[cfg(target_arch = "riscv64")] {
+        mod riscv64;
+        pub use self::riscv64::*;
     }
 }
