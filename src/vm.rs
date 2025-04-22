31d787c3009713178ada99038dd580624af1a089
Su Mingxian <aarkegz@gmail.com>
2025-04-22 14:52:26 +0800

    Add shutting down flag for `AxVM` (#18)
    * add shutting down flag for `AxVM`
    
    * formatted
    
    * make CI continue-on-error on nightly toolchain

--- src/vm.rs
+++ src/vm.rs
@@ -47,6 +47,7 @@ struct AxVMInnerMut<H: AxVMHal> {
 /// A Virtual Machine.
 pub struct AxVM<H: AxVMHal, U: AxVCpuHal> {
     running: AtomicBool,
+    shutting_down: AtomicBool,
     inner_const: AxVMInnerConst<U>,
     inner_mut: AxVMInnerMut<H>,
 }
@@ -175,6 +176,7 @@ impl<H: AxVMHal, U: AxVCpuHal> AxVM<H, U> {
 
             Self {
                 running: AtomicBool::new(false),
+                shutting_down: AtomicBool::new(false),
                 inner_const: AxVMInnerConst {
                     id: config.id(),
                     config,
@@ -188,7 +190,7 @@ impl<H: AxVMHal, U: AxVCpuHal> AxVM<H, U> {
             }
         });
 
-        info!("VM created: id={}", result.id());
+        info!("VM[{}] created", result.id());
 
         // Setup VCpus.
         for vcpu in result.vcpu_list() {
@@ -203,7 +205,7 @@ impl<H: AxVMHal, U: AxVCpuHal> AxVM<H, U> {
                 <AxArchVCpuImpl<U> as AxArchVCpu>::SetupConfig::default(),
             )?;
         }
-        info!("VM setup: id={}", result.id());
+        info!("VM[{}] vcpus set up", result.id());
 
         Ok(result)
     }
@@ -268,13 +270,38 @@ impl<H: AxVMHal, U: AxVCpuHal> AxVM<H, U> {
         if !has_hardware_support() {
             ax_err!(Unsupported, "Hardware does not support virtualization")
         } else if self.running() {
-            ax_err!(BadState, format!("VM[{}] is running", self.id()))
+            ax_err!(BadState, format!("VM[{}] is already running", self.id()))
         } else {
             info!("Booting VM[{}]", self.id());
             self.running.store(true, Ordering::Relaxed);
             Ok(())
         }
     }
+
+    /// Returns if the VM is shutting down.
+    pub fn shutting_down(&self) -> bool {
+        self.shutting_down.load(Ordering::Relaxed)
+    }
+
+    /// Shuts down the VM by setting the shutting_down flag as true.
+    ///
+    /// Currently, the "re-init" process of the VM is not implemented. Therefore, a VM can only be
+    /// booted once. And after the VM is shut down, it cannot be booted again.
+    pub fn shutdown(&self) -> AxResult {
+        if self.shutting_down() {
+            ax_err!(
+                BadState,
+                format!("VM[{}] is already shutting down", self.id())
+            )
+        } else {
+            info!("Shutting down VM[{}]", self.id());
+            self.shutting_down.store(true, Ordering::Relaxed);
+            Ok(())
+        }
+    }
+
+    // TODO: implement suspend/resume.
+    // TODO: implement re-init.
 
     /// Returns this VM's emulated devices.
     pub fn get_devices(&self) -> &AxVmDevices {
