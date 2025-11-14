77357c2fab93bc3133b9953790b8092d2e740aed
周睿 <zrufo747@outlook.com>
2025-11-14 12:53:40 +0800

    更新 Cargo.toml，添加 axhal、axruntime 和 fdt-parser 依赖；重构 AArch64 架构的 cpu 模块，新增 vhal 模块以支持虚拟化功能

--- src/fdt/mod.rs
+++ src/fdt/mod.rs
@@ -1,1 +1,29 @@
+use alloc::vec::Vec;
+use fdt_parser::{Fdt, Status};
 
+pub(crate) fn fdt() -> Option<Fdt> {
+    let addr = axhal::get_bootarg();
+    if addr == 0 {
+        return None;
+    }
+    let fdt = unsafe { Fdt::from_ptr(addr as *mut u8).ok()? };
+    Some(fdt)
+}
+
+pub fn cpu_list() -> Option<Vec<usize>> {
+    let fdt = fdt()?;
+
+    let nodes = fdt.find_nodes("/cpus/cpu");
+    let cpus = nodes
+        .into_iter()
+        .filter(|node| node.name().contains("cpu@"))
+        .filter(|node| !matches!(node.status(), Some(Status::Disabled)))
+        .map(|node| {
+            let reg = node
+                .reg()
+                .unwrap_or_else(|_| panic!("cpu {} reg not found", node.name()))[0];
+            reg.address as usize
+        })
+        .collect();
+    Some(cpus)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,9 @@ mod vm;
 mod vm2;
 
 pub mod config;
+pub mod vhal;
 
+use anyhow::bail;
 pub use hal::AxVMHal;
 pub use vm::AxVCpuRef;
 pub use vm::AxVM;
@@ -38,3 +40,4 @@ pub type AxVMPerCpu<U> = axvcpu::AxPerCpu<vcpu::AxVMArchPerCpuImpl<U>>;
 pub fn has_hardware_support() -> bool {
     vcpu::has_hardware_support()
 }
+
